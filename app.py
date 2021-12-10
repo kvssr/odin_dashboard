d2c37b531f277915542ee666173159df45838625
kvssr <kevin.visser.info@gmail.com>
2021-12-10 16:33:21 +0100

    Added server in app

--- app.py
+++ app.py
@@ -16,6 +16,8 @@ external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
+server = app.server
+
 theme = {
     'dark': True,
     'detail': '#007439',
