d8d15a635f03a1757f73d1fabb7fb545a19278cb
Spalarttheo <spalart.theo@gmail.com>
2021-12-11 16:36:55 +0100

    Dark mode and visual updates

--- app.py
+++ app.py
@@ -19,34 +19,63 @@ app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 server = app.server
 
 profession_colours = {
-                             'Guardian': '#72C1D9',
-                             'Dragonhunter': '#72C1D9',
-                             'Firebrand': '#72C1D9',
-                             'Revenant': '#D16E5A',
-                             'Herald': '#D16E5A',
-                             'Renegade': '#D16E5A',
-                             'Warrior': '#FFD166',
-                             'Berserker': '#FFD166',
-                             'Spellbreaker': '#FFD166',
-                             'Engineer': '#D09C59',
-                             'Scrapper': '#D09C59',
-                             'Holosmith': '#D09C59',
-                             'Ranger': '#8CDC82',
-                             'Druid': '#8CDC82',
-                             'Soulbeast': '#8CDC82',
-                             'Thief': '#C08F95',
-                             'Daredevil': '#C08F95',
-                             'Deadeye': '#C08F95',
-                             'Elementalist': '#F68A87',
-                             'Tempest': '#F68A87',
-                             'Weaver': '#F68A87',
-                             'Mesmer': '#B679D5',
-                             'Chronomancer': '#B679D5',
-                             'Mirage': '#B679D5',
-                             'Necromancer': '#52A76F',
-                             'Reaper': '#52A76F',
-                             'Scourge': '#52A76F',
+                             'Guardian': '#186885',
+                             'Dragonhunter': '#186885',
+                             'Firebrand': '#186885',
+                             'Revenant': '#661100',
+                             'Herald': '#661100',
+                             'Renegade': '#661100',
+                             'Warrior': '#CAAA2A',
+                             'Berserker': '#CAAA2A',
+                             'Spellbreaker': '#CAAA2A',
+                             'Engineer': '#87581D',
+                             'Scrapper': '#87581D',
+                             'Holosmith': '#87581D',
+                             'Ranger': '#67A833',
+                             'Druid': '#67A833',
+                             'Soulbeast': '#67A833',
+                             'Thief': '#974550',
+                             'Daredevil': '#974550',
+                             'Deadeye': '#974550',
+                             'Elementalist': '#DC423E',
+                             'Tempest': '#DC423E',
+                             'Weaver': '#DC423E',
+                             'Mesmer': '#69278A',
+                             'Chronomancer': '#69278A',
+                             'Mirage': '#69278A',
+                             'Necromancer': '#2C9D5D',
+                             'Reaper': '#2C9D5D',
+                             'Scourge': '#2C9D5D',
                          }
+profession_shorts = {
+    'Guardian': 'Gnd',
+    'Dragonhunter': 'Dgh',
+    'Firebrand': 'Fbd',
+    'Revenant': 'Rev',
+    'Herald': 'Her',
+    'Renegade': 'Ren',
+    'Warrior': 'War',
+    'Berserker': 'Brs',
+    'Spellbreaker': 'Spb',
+    'Engineer': 'Eng',
+    'Scrapper': 'Scr',
+    'Holosmith': 'Hls',
+    'Ranger': 'Rgr',
+    'Druid': 'Dru',
+    'Soulbeast': 'Slb',
+    'Thief': 'Thf',
+    'Daredevil': 'Dar',
+    'Deadeye': 'Ded',
+    'Elementalist': 'Ele',
+    'Tempest': 'Tmp',
+    'Weaver': 'Wea',
+    'Mesmer': 'Mes',
+    'Chronomancer': 'Chr',
+    'Mirage': 'Mir',
+    'Necromancer': 'Nec',
+    'Reaper': 'Rea',
+    'Scourge': 'Scg',
+}
 
 
 app.layout = html.Div([
@@ -72,6 +101,8 @@ app.layout = html.Div([
     html.Div(id='output-data-upload'),
 ])
 
+def get_short_profession(profession):
+    return "("+profession_shorts[profession]+")"
 
 def parse_contents(contents, filename, date):
     content_type, content_string = contents.split(',')
@@ -84,10 +115,17 @@ def parse_contents(contents, filename, date):
         elif 'xls' in filename:
             # Assume that the user uploaded an excel file
             df = pd.read_excel(io.BytesIO(decoded), sheet_name='dmg')
+            for index, row in df.iterrows():
+                df.at[index,'Name'] = "{:<25}".format(row['Name'])+get_short_profession(row['Profession'])+" "
             fig = px.bar(df, y="Name", x="Total dmg", color="Profession", text="Total dmg", barmode="relative",
                          orientation='h', height=1000,
                          color_discrete_map=profession_colours)
-            fig.update_layout(yaxis_categoryorder='total ascending')
+            fig.update_layout(
+                yaxis_categoryorder='total ascending',
+                paper_bgcolor='rgba(0,0,0,0)',
+                plot_bgcolor='rgba(0,0,0,0)',
+                font_color='#EEE'
+            )
             #adds the labels for dmg/s and times top/attendance
             for name in df["Name"]:
                 fig.add_annotation(y=name, x=df[df["Name"] == name]["Total dmg"].values[0],
@@ -97,7 +135,7 @@ def parse_contents(contents, filename, date):
                                    xshift=0,
                                    xanchor="left"),
                 fig.add_annotation(y=name, x=0,
-                                   text=str(int(df[df["Name"] == name]["Times Top"].values[0]))
+                                   text=" "+str(int(df[df["Name"] == name]["Times Top"].values[0]))
                                         + " / " +
                                         str(int(df[df["Name"] == name]["Attendance (number of fights)"].values[0])),
                                    showarrow=False,
