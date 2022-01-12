0b0188a922e1fd69907d20650c674490dc3b01a6
kvssr <kevin.visser.info@gmail.com>
2022-01-12 13:38:24 +0100

    Added better titles

--- apps/top_stats.py
+++ apps/top_stats.py
@@ -16,13 +16,13 @@ from app import app, db
 from models import CleanseStat, DistStat, DmgStat, Fight, FightSummary, HealStat, RipStat, StabStat
 
 
-def get_fig_with_model(model, t, limit):
+def get_fig_with_model(model, t, title, limit):
     try:
         dmg_list = db.session.query(model).limit(limit).all()
 
         if dmg_list:
             df = pd.DataFrame([s.to_dict() for s in dmg_list])
-            fig = graphs.get_top_bar_chart(df, t)
+            fig = graphs.get_top_bar_chart(df, t, title)
             return fig
     except Exception as e:
         print(e)        
@@ -60,7 +60,7 @@ layout = html.Div(children=[
             dbc.Col(
                 dcc.Graph(
                     id='top-dmg-chart',
-                    figure=get_fig_with_model(DmgStat, 'dmg', 5)
+                    figure=get_fig_with_model(DmgStat, 'dmg', 'Top Damage', 5)
                 ), md=12, lg=6, className='bar-chart'),     
             dbc.Col(
                 dcc.Graph(
@@ -72,24 +72,24 @@ layout = html.Div(children=[
             dbc.Col(
                 dcc.Graph(
                     id='top-stab-chart',
-                    figure=get_fig_with_model(StabStat, 'stab', 3)
+                    figure=get_fig_with_model(StabStat, 'stab', 'Top Stability Output', 3)
                 ), md=12, lg=6, className='bar-chart'),     
             dbc.Col(
                 dcc.Graph(
                     id='top-cleanses-chart',
-                    figure=get_fig_with_model(CleanseStat, 'cleanses', 3)
+                    figure=get_fig_with_model(CleanseStat, 'cleanses', 'Top Condition Cleanse', 3)
                 ), md=12, lg=6, className='bar-chart')
         ]),
         dbc.Row([
             dbc.Col(
                 dcc.Graph(
                     id='top-heal-chart',
-                    figure=get_fig_with_model(HealStat, 'heal', 3)
+                    figure=get_fig_with_model(HealStat, 'heal', 'Top Healing Output', 3)
                 ), md=12, lg=6, className='bar-chart'),     
             dbc.Col(
                 dcc.Graph(
                     id='top-rips-chart',
-                    figure=get_fig_with_model(RipStat, 'rips', 3)
+                    figure=get_fig_with_model(RipStat, 'rips', 'Top Boon Removal', 3)
                 ), md=12, lg=6, className='bar-chart')
         ]),
         ])
