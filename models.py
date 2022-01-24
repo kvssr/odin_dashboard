7b8315b63d91b27c9764049966f02d91fa38c6b1
kvssr <kevin.visser.info@gmail.com>
2022-01-24 18:51:13 +0100

    Added user table

--- models.py
+++ models.py
@@ -1,5 +1,6 @@
 from sqlalchemy.orm import relationship
 from app import db
+from flask_login import UserMixin
 
 
 class Character(db.Model):
@@ -537,3 +538,15 @@ class FightSummary(db.Model):
             'Healing': f'{self.healing:,}',
             'Damage Taken': f'{self.dmg_taken:,}',
         }
+
+
+class User(UserMixin, db.Model):
+    __tablename__ = 'user'
+
+    id = db.Column(db.Integer(), autoincrement=True, primary_key=True)
+    username = db.Column(db.String(), unique= True)
+    password = db.Column(db.String())
+    email = db.Column(db.String())
+
+    def __init__(self, username):
+        self.username = username
